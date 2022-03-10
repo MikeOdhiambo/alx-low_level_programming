745718dca2b1e8f90693b6ca476f535880c8adb8
MikeOdhiambo <odhiambomike423@gmail.com>
2022-03-10 15:14:21 -0800

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+/* Print to standard output using printf */
+#include<stdio.h>
+
+/* Entry point */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
