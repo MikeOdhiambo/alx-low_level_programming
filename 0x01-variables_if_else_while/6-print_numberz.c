efe9e6c0be51567d33b4b6ad78d5876b8527db9f
MikeOdhiambo <odhiambomike423@gmail.com>
2022-03-11 15:21:13 -0800

    Solution 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Print numbers using putchar only
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
+	return (0);
+}
