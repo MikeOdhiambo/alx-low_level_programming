2b4abdd82de0e49d8424c5bee1815e5920e6045c
MikeOdhiambo <odhiambomike423@gmail.com>
2022-03-20 08:39:26 -0700

    Optional task 1

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - print largest prime factor
+*
+* Return: void
+*/
+
+int main(void)
+{
+	unsigned long i;
+	unsigned long num = 612852475143;
+
+
+	while (num % 2 == 0)
+		num = num / 2;
+	for (i = 3; i <= 999999; i = i + 2)
+	{
+		while (num % i == 0)
+		{
+			num = num / i;
+		}
+	}
+
+	if (num > 2)
+		printf("%lu\n", num);
+	return (0);
+}
