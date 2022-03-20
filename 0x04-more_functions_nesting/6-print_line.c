5655cb90eff72f90c707c271df1139b637ebc128
MikeOdhiambo <odhiambomike423@gmail.com>
2022-03-20 07:14:43 -0700

    More functions work

---
+++ 0x04-more_functions_nesting/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - print line in the terminal
+ * @n: number of dashes
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+	while (n > 0)
+	{
+		_putchar('_');
+		n--;
+	}
+	_putchar('\n');
+}
