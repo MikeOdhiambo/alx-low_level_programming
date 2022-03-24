d6590738161136e423ba10a3f432e163b439e96a
MikeOdhiambo <odhiambomike423@gmail.com>
2022-03-24 16:51:26 -0700

    Changed initial file

--- 0x06-pointers_arrays_strings/100-roti13.c
+++ 0x06-pointers_arrays_strings/100-roti13.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* rot13 - encode a string using rot13
+* rot13 - encode string using roti13
 * @a: string to encode
 *
 * Return: encoded string
@@ -10,7 +10,7 @@
 char *rot13(char *a)
 {
 	int i, j;
-	char alphabet[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+	char abc[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c'
 , 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
@@ -22,7 +22,7 @@ char *rot13(char *a)
 	for (i = 0; a[i]; i++)
 	{
 		for (j = 0; j <= 52; j++)
-			if (a[i] == alphabet[j])
+			if (a[i] == abc[j])
 			{
 				a[i] = rot13[j];
 				break;
