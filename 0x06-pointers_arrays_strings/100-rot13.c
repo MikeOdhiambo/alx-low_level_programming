0ef9b87d55a6db3d995c473a4f6267f67f977aba
MikeOdhiambo <odhiambomike423@gmail.com>
2022-03-24 17:17:58 -0700

    Solution

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * rot13 - encode a string using rot13 encoding
+ * @a: String to encode
+ *
+ * Return: encoded string
+ */
+
+char *rot13(char *a)
+{
+	int i, j;
+	char abc[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c'
+, 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+'s', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	char rot13[] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'n', 'o'
+, 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd',
+'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'};
+
+	for (i = 0; a[i]; i++)
+	{
+		for (j = 0; j <= 52; j++)
+			if (a[i] == abc[j])
+			{
+				a[i] = rot13[j];
+				break;
+			}
+	}
+	return (a);
+}
