6a542460a9a9e0630054d0db2dbffbeee6c82d75
MikeOdhiambo <odhiambomike423@gmail.com>
2022-03-30 10:36:26 -0700

    Advanced task 1

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * _strlen - calculate lenght of a string
+ * @s: string.
+ * Return: length of string.
+ */
+int _strlen(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return (1 + _strlen(s + 1));
+}
+/**
+ * palindrome - checks if a string is palindrome
+ * @s1: first character of string.
+ * @s2: last character of string.
+ * Return: 1 if s is palindrome, 0 if not.
+ */
+int palindrome(char *s1, char *s2)
+{
+	if (s1 >= s2)
+		return (1);
+	if (*s1 != *s2)
+		return (0);
+	return (palindrome(s1 + 1, s2 - 1));
+}
+
+/**
+ * is_palindrome - checks if a string is palindrome
+ * @s: string to test.
+ * Return: 1 if s is palindrome, 0 if not.
+ */
+int is_palindrome(char *s)
+{
+	return (palindrome(s, s + _strlen(s) - 1));
+}
+
