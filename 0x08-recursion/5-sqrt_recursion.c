988fe75cf864d561173f6b7fadf248b0218325a4
MikeOdhiambo <odhiambomike423@gmail.com>
2022-03-30 07:10:44 -0700

    Fix

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,12 +3,12 @@
 /**
  * _sqrt_recursion - Finds and returns the natural square root
  * of a number
+ *
  * @n: Given integer
  *
  * Return: The square root or -1 if no square root
  */
 
-int root(int i, int j);
 
 int _sqrt_recursion(int n)
 {
@@ -16,12 +16,14 @@ int _sqrt_recursion(int n)
 }
 
 /**
- * root - Approximates the square root recursively
+ * root - Recursively approximates the square root
+ *
  * @i: checker value
  * @j: squared value
  *
  * Return: int
  */
+
 int root(int i, int j)
 {
 	if (i > j)
