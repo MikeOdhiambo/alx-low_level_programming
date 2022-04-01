601ddde0356d1a81f33e717f864de984347a9e74
MikeOdhiambo <odhiambomike423@gmail.com>
2022-04-01 15:08:38 -0700

    Solution 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Prints the name of the program folowed by a new line
+ * @argc: Number of command line arguments
+ * @argv: Array of pointers to arguments
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
