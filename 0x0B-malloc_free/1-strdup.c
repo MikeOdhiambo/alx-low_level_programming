d822efa686e2f3ac47e17a1387c82a8b53f3fb41
MikeOdhiambo <odhiambomike423@gmail.com>
2022-04-05 12:17:05 -0700

    Solution 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+
+#include "main.h"
+/**
+ * *_strdup - copies a string to newly allocated space
+ * @str: string to be copied
+ *
+ * Return: pointer (Success), NULL (failure)
+ */
+char *_strdup(char *str)
+{
+	int i, j;
+	char *ptr;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; str[i]; i++)
+	{
+	}
+	ptr = malloc(sizeof(char) * i);
+	if (ptr != NULL)
+	{
+		for (j = 0; j < i; j++)
+		{
+			ptr[j] = str[j];
+		}
+		ptr[j] = '\0';
+	}
+	return (ptr);
+}
