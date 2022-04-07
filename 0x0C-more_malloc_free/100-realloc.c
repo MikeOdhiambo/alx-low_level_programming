ae8652273459789b6e4d4dc87ec3f0e62d619452
MikeOdhiambo <odhiambomike423@gmail.com>
2022-04-07 15:36:56 -0700

    Removed min function

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,61 +1,46 @@
 #include <stdlib.h>
 
 #include "main.h"
-/**
- * min - return the minimum of two positive integers
- * @a: first number
- * @b: second number
- *
- * Return: minimum int
- */
-int min(unsigned int a, unsigned int b)
-{
-	if (a < b)
-	{
-		return (a);
-	}
-	return (b);
-}
 
 /**
- * *_realloc - Reallocates a memory block using malloc
- * @ptr: pointer to old memory block
- * @old_size: old buffer size
- * @new_size: new buffer size
+ * *_realloc - reallocates memory using malloc
+ * @ptr: pointer to the peviously allocated memory
+ * @old_size: old size
+ * @new_size: new size for memory block
  *
- * Return: new pointer (Success), NULL (Fail)
+ * Return: pointer (Success), NULL (failure)
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *newptr, *oldptr;
 	unsigned int i;
-	int buff;
 
-	if (ptr == NULL)
-	{
-		return (malloc(new_size));
-	}
+	if (new_size == old_size)
+		return (ptr);
+
 	if (ptr && new_size == 0)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	if (new_size == old_size)
-	{
-		return (ptr);
-	}
+
+	if (!ptr)
+		return (malloc(new_size));
+
 	oldptr = ptr;
-	buff = min(old_size, new_size);
+
 	newptr = malloc(new_size);
-	if (newptr == NULL)
-	{
-		free(newptr);
+
+	if (!newptr)
 		return (NULL);
-	}
-	for (i = 0; i < buff; i++)
-	{
-		newptr[i] = oldptr[i];
-	}
+
+	if (new_size < old_size)
+		for (i = 0; i < new_size; i++)
+			newptr[i] = oldptr[i];
+	else
+		for (i = 0; i < old_size; i++)
+			newptr[i] = oldptr[i];
+
 	free(ptr);
 	return (newptr);
 }
