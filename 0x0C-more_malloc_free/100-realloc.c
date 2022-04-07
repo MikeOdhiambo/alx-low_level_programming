a974d861ce051153a22e4327e2727c652bc0e7e3
MikeOdhiambo <odhiambomike423@gmail.com>
2022-04-07 14:40:25 -0700

    Advanced task 100 solution

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,57 @@
+#include <stdlib.h>
+
+#include "main.h"
+/**
+ * min - return the minimum of two positive integers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: minimum int
+ */
+int min(unsigned int a, unsigned int b)
+{
+	if (a < b)
+	{
+		return (a);
+	}
+	return (b);
+}
+
+/**
+ * *_realloc - Reallocates a memory block using malloc
+ * @ptr: pointer to old memory block
+ * @old_size: old buffer size
+ * @new_size: new buffer size
+ *
+ * Return: new pointer (Success), NULL (Fail)
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *newptr, *oldptr;
+	unsigned int i;
+	int buff;
+
+	oldptr = ptr;
+	if (ptr == NULL)
+	{
+		newptr = malloc(new_size);
+		return (newptr);
+	}
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	buff = min(old_size, new_size);
+	newptr = malloc(new_size);
+	if (newptr == NULL)
+	{
+		free(newptr);
+		return (NULL);
+	}
+	for (i = 0; i < buff; i++)
+	{
+		newptr[i] = oldptr[i];
+	}
+	return (newptr);
+}
