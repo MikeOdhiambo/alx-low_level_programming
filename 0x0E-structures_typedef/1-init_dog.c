e934495a5b8192a6e920cc60c57b352abc2f9bbf
MikeOdhiambo <odhiambomike423@gmail.com>
2022-04-11 12:48:19 -0700

    Add pointer declaration

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,7 +11,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	struct dog *ptr;
+
+	ptr = d;
+	ptr->name = name;
+	ptr->age = age;
+	ptr->owner = owner;
 }
