fd90c26428382c41aedce9030cf1102182f64db0
MikeOdhiambo <odhiambomike423@gmail.com>
2022-04-11 13:25:25 -0700

    Add prototype to header file

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,14 +11,11 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog *ptr;
-
-	ptr = d;
-	if (ptr == NULL)
+	if (d == NULL)
 	{
 		return;
 	}
-	ptr->name = name;
-	ptr->age = age;
-	ptr->owner = owner;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
