54f3e7d15f2320b80980d4337c386c1d41368be8
MikeOdhiambo <odhiambomike423@gmail.com>
2022-04-11 14:40:01 -0700

    Add dog.h and README

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a collection of dog properties
+ * @name: str name of dog
+ * @age: float age of dog
+ * @owner: str owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *name;
+}
+
+#endif
