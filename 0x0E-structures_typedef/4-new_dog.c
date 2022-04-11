784bc3276d9c48f28a75e76675b0c8b1bb5c3073
MikeOdhiambo <odhiambomike423@gmail.com>
2022-04-11 16:26:46 -0700

    Store name and owner copies

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,6 +12,35 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t ndog, *ptr;
+	char *namecpy, *ownercpy;
+	int i, j, len;
+
+	if (name == NULL)
+	{
+		return;
+	}
+	if (owner == NULL)
+	{
+		return;
+	}
+	for (i = 0; name[i]; i++)
+	{
+	}
+	for (j = 0; owner[j]; j++)
+	{
+	}
+	namecpy = malloc(sizeof(char) * (i + 1));
+	ownercpy = malloc(sizeof(char) * (j + 1));
+	for (len = 0; len < i; len++)
+	{
+		namecpy[len] = name[len];
+	}
+	for (len = 0; len < j; len++)
+	{
+		ownercpy[len] = owner[len];
+	}
+	namecpy[len] = '\0';
+	ownercpy[len] = '\0';
 
 	ptr = &ndog;
 	if (ptr == NULL)
