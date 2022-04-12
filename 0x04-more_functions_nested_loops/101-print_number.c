9e3a20bb0778e7970155a0d8f7c297d31eeeb73f
MikeOdhiambo <odhiambomike423@gmail.com>
2022-04-12 15:33:54 -0700

    Add advanced task 1

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,40 +1,26 @@
 #include "main.h"
 
 /**
- * print_number - print exact integer
- * @n: number to print
- *
- * Return: void
+ * print_number - prints an integer
+ * @n: integer to be printed
  */
-
 void print_number(int n)
 {
+	unsigned int n1;
 
-	unsigned int power;
-	int printing = 0;
-	unsigned int nosign;
-
-	if (n == 0)
-		_putchar ('0');
-	else
+	if (n < 0)
 	{
-		if (n < 0)
-		{
-			n = n * -1;
-			nosign = n;
-			_putchar ('-');
-		}
-		else
-			nosign = n;
-		for (power = 1000000000; power >= 1; power = power / 10)
-		{
-			if (nosign >= power)
-				printing = 1;
-			if (printing == 1)
-			{
-				_putchar ((nosign / power) + '0');
-				nosign = nosign % power;
-			}
-		}
+		n1 = -n;
+		_putchar('-');
+	} else
+	{
+		n1 = n;
 	}
+
+	if (n1 / 10)
+	{
+		print_number(n1 / 10);
+	}
+
+	_putchar((n1 % 10) + '0');
 }
