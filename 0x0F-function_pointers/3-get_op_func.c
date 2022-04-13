5dd490b297cdd62790d0433ab50d7809a5c44bb3
MikeOdhiambo <odhiambomike413@gmail.com>
2022-04-13 15:50:11 -0700

    Edit while loop

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -22,11 +22,11 @@ int (*get_op_func(char *s))(int, int)
 	i = 0;
 	while (i < 5)
 	{
-		if (ops[i].op[0] == s[0])
+		if (*(ops[i].op) != *s)
 		{
-			return (ops[i].f);
+			return (NULL);
 		}
 		i++;
 	}
-	return (NULL);
+	return (ops[i].f);
 }
