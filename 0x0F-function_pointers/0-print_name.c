fce3a1c8059422291744f88495bbb0ca87f3a42f
MikeOdhiambo <odhiambomike423@gmail.com>
2022-04-13 06:27:24 -0700

    Changed if condition

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -9,13 +9,8 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL)
+	if (name != NULL && f != NULL)
 	{
-		return;
+		f(name);
 	}
-	if (f == NULL)
-	{
-		return;
-	}
-	f(name);
 }
