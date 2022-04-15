07ca198e579cfe967bf644040ee4c7d9ed057f44
MikeOdhiambo <odhiambomike413@gmail.com>
2022-04-14 18:11:32 -0700

    Ssolution 2

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,39 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - print variable string params
+ * @separator: separator between strings
+ * @n: number of params
+ *
+ * Return: void
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list strlist;
+	char *ps;
+
+	va_start(strlist, n);
+	for (i = 0; i < n; i++)
+	{
+		ps = va_arg(strlist, const char *s);
+		if (!ps)
+		{
+			ps = "(nil)";
+		}
+		if (!separator)
+		{
+			printf("%s", ps);
+		}
+		else if (separator && i == 0)
+		{
+			printf("%s", ps);
+		}
+		else
+		{
+			printf("%s%s", separator, ps);
+		}
+	}
+	printf("\n");
+	va_end(strlist);
+}
