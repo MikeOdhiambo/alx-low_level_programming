f22c277ac9897871152624305d0dce536c1ee329
MikeOdhiambo <odhiambomike413@gmail.com>
2022-04-26 10:25:10 -0700

    Remove initial checks

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,10 +13,6 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int i;
 	listint_t *ptr;
 
-	if (!head || !index)
-	{
-		return (NULL);
-	}
 	ptr = head;
 	for (i = 0; ptr && i < index; i++)
 	{
