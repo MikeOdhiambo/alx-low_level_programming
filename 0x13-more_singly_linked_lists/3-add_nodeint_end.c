f4923c97d02b12b52f4bcd1d86a2c2052074ae64
MikeOdhiambo <odhiambomike413@gmail.com>
2022-04-26 09:30:41 -0700

    Assign ptr variable

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -14,6 +14,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	node = malloc(sizeof(listint_t));
 	node->n = n;
 	node->next = NULL;
+	ptr = *head;
 	if (ptr == NULL)
 	{
 		ptr = node;
