0c8a8320cd102af8a7d11b908524463457bbdcac
MikeOdhiambo <odhiambomike413@gmail.com>
2022-04-26 12:49:07 -0700

    Correct typos

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -6,11 +6,11 @@
  * @idx: Index of insertion
  * @n: Data value of node
  *
- * Return: Ppointer to new node, NULL (Fail)
+ * Return: Pointer to new node, NULL (Fail)
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint *node, *ptr;
+	listint_t *node, *ptr;
 	unsigned int i;
 
 	node = malloc(sizeof(listint_t));
@@ -20,7 +20,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	node->n = n;
 	node->next = NULL;
-	if (!(*head) || !idx)
+	if (!(*head))
 	{
 		return (NULL);
 	}
