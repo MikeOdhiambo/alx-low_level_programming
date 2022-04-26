60f2396dec5a13d212ac0c43880b271427d67436
MikeOdhiambo <odhiambomike413@gmail.com>
2022-04-26 12:46:43 -0700

    Redo enire function

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,16 +1,16 @@
 #include "lists.h"
 
 /**
- * *insert_nodeint_at_index - Inserts a node at a given position
+ * *insert_nodeint_at_index - Insert node at given index
  * @head: Pointer to pointer of head node
- * @idx: Insertion point
+ * @idx: Index of insertion
  * @n: Data value of node
  *
- * Return: Pointer to new node, NULL (Fail)
+ * Return: Ppointer to new node, NULL (Fail)
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_int *node, *ptr;
+	listint *node, *ptr;
 	unsigned int i;
 
 	node = malloc(sizeof(listint_t));
@@ -20,13 +20,27 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	node->n = n;
 	node->next = NULL;
-	ptr = *head;
-	for (i = 0; i < idx - 1; i++)
+	if (!(*head) || !idx)
 	{
+		return (NULL);
+	}
+	ptr = *head;
+
+	if (idx == 0)
+	{
+		node->next = *head;
+		*head = node;
+		return (node);
+	}
+	for (i = 0; i < idx && ptr; i++)
+	{
+		if (i == (idx - 1))
+		{
+			node->next = ptr->next;
+			ptr->next = node;
+			return (node);
+		}
 		ptr = ptr->next;
 	}
-	node->next = ptr->next;
-	ptr->next = node;
-
-	return (node);
+	return (NULL);
 }
