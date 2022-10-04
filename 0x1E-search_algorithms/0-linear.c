d3d8a3aa441613c43f9fbe588be35c19a07cfbf8
MikeOdhiambo <odhiambomike413@gmail.com>
2022-10-04 21:23:54 +0300

    Type cast size variable

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,5 +1,5 @@
 #include "search_algos.h"
-
+#include <stdlib.h>
 /**
  * linear_search - Searches for a value using linear search algorithm
  * @array: Array to search the value in
@@ -9,9 +9,11 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
+	int i;
+
 	if (!array)
 		return (-1);
-	for (int i = 0; i < size; i++)
+	for (i = 0; i < (int)size; i++)
 	{
 		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 		if (array[i] == value)
