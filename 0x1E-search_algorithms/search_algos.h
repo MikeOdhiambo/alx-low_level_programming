a0a5091375025719700573f361a8900c415aeedc
MikeOdhiambo <odhiambomike413@gmail.com>
2022-10-04 03:21:55 +0300

    Update prototypes

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,12 @@
+#ifndef SEARCH_ALGOS
+#define SEARCH_ALGOS
+
+/* INCLUDE LIBRARIES */
+#include <stdio.h>
+
+/* PROTOTYPES */
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int _round(double x);
+
+#endif
