09f40e60c9113880bc9c8c7a6127e68c86d6c097
MikeOdhiambo <odhiambomike413@gmail.com>
2022-10-05 21:38:40 +0300

    Remove main function

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -53,7 +53,7 @@ int jump_search(int *array, size_t size, int value)
 	return (-1);
 }
 
-int main(void)
+/*int main(void)
 {
     int array[] = {
         0, 1, 2, 3, 4, 5, 6, 7, 8, 9
@@ -64,4 +64,4 @@ int main(void)
     printf("Found %d at index: %d\n\n", 1, jump_search(array, size, 1));
     printf("Found %d at index: %d\n", 999, jump_search(array, size, 999));
     return (EXIT_SUCCESS);
-}
+}*/
