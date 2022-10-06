e5811a445229fe64365ce3da4d848788139ed559
MikeOdhiambo <odhiambomike413@gmail.com>
2022-10-06 03:25:10 +0300

    Rewrite function

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,63 +1,40 @@
-#include "search_algos.h"
 #include <math.h>
-#include <stdlib.h>
+#include "search_algos.h"
 
 /**
- * _round - Rounds up a number
- * @x: Number to be rounded up
- * Return: int
+ * jump_search - Search through an array using Jump Search Algorithm
+ * @array: Pointer to array to search through
+ * @size: Length of array
+ * @value: Value to be searched
+ *
+ * Return: Index of found value (Success), -1 (Fail)
  */
 
-int _round(double x)
-{
-	if (x < 0.0)
-		return ((int)(x - 0.5));
-	else
-		return ((int)(x + 0.5));
-}
-
-/**
- * jump_search - Searches for value in array using jump search algorithm
- * @array: Array to search the value in
- * @size: Length of the array
- * @value: Number to be searched
- * Return: The index where the value is located or -1 if not found
- */
 int jump_search(int *array, size_t size, int value)
 {
-	int i = 0, j, sub_size;
-	int m = _round(sqrt((int)size));
-	int prev;
+	size_t i = 0, j;
+	size_t m = sqrt(size);
+	size_t step = i * m;
 
-	if (!array)
+	if (!array || size == 0)
 		return (-1);
 
-	while ((i * m) <= (int)size)
+	while (step <= size)
 	{
-		prev = (i * m) - m < 0 ? 0 : (i * m) - m;
-		if (array[i * m] >= value)
+		if (array[step] >= value)
 		{
-			printf("Value found between indexes [%d] and [%d]\n", prev, (i * m));
-			sub_size = (i * m) - prev;
-			for (j = 0; j <= sub_size; j++)
+			printf("Value found between indexes [%ld] and [%ld]\n", step - m, step);
+			for (j  = step - m; j <= step; j++)
 			{
-				printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
-				if (array[prev] == value)
-					return (prev);
-				prev += 1;
+				printf("Value checked array[%ld] = [%d]\n", j, array[j]);
+				if (array[j] == value)
+					return (j);
 			}
 			break;
 		}
-		printf("Value checked array[%d] = [%d]\n", i * m, array[i * m]);
+		printf("Value checked array[%ld] = [%d]\n", step, array[step]);
 		i += 1;
+		step = i * m;
 	}
-	prev = (i * m) - m;
-	printf("Value found between indexes [%d] and [%d]\n", prev, (i * m));
-	for (j = prev; j <= (int)size; j++)
-	{
-		printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
-		j += 1;
-	}
-
 	return (-1);
 }
